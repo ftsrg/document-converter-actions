8266054a41836e603902ce5f045d710bf127b21a
Gabor Szarnyas <szarnyasg@gmail.com>
2020-03-31 23:47:50 +0200

    Cleanup output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/sh -l
 
-echo ">>> Hello from inside the container"
-echo ">>> Let's build"
-
 cd /github/workspace
 make
-
-echo ">>> PDFs built"
 
 time=$(date)
 echo ::set-output name=time::$time
